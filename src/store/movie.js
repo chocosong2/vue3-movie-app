02a1653e2bbbf8a2bc6463a98baa0f41b67352a4
chocosong2 <greentea030@naver.com>
2026-06-19 16:40:24 +0900

    영화 검색 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -14,7 +14,7 @@ export default {
     ,
     //methods
     mutations:{
-        updatestate(state,payload){
+        updateState(state,payload){
             Object.keys(payload).forEach(key => {
                 state[key] = payload[key]
                 
@@ -25,14 +25,30 @@ export default {
         }
     },
     actions:{
-        async searchMovies({commit}, payload){
+        async searchMovies({state, commit}, payload){
             const {title, type, number, year} = payload
             const OMDB_API_KEY = '7035c60c' 
             const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
             const { Search, totalResults} = res.data
-            commit('updatestate',{
+            commit('updateState',{
                 movies:Search
             })
+            const total = parseInt(totalResults, 10)
+            const pageLength = Math.ceil(total / 10)
+
+            if (pageLength > 1){
+                for(let page=2; page <= pageLength; page +=1){
+                    if (page > (number / 10)){
+                        break
+                    }
+                    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+                    const {Search} = res.data 
+                    commit('updateState', {
+                        movies: [...state.movies, ...Search]
+                    })
+                }
+            }
+
         }
     },
 }
