68f61ae6387e62f7dad1ae7d83db67739ffb0486
chocosong2 <greentea030@naver.com>
2026-06-19 15:50:47 +0900

    movie search

---
+++ src/store/movie.js
@@ -0,0 +1,38 @@
+
+import axios from 'axios'
+export default {
+    //module 
+    namespaced :true,
+    //data
+    state: () => ({
+        movies:[],
+        message:'',
+        loading: false
+    }),
+    //computed
+    getters:{}
+    ,
+    //methods
+    mutations:{
+        updatestate(state,payload){
+            Object.keys(payload).forEach(key => {
+                state[key] = payload[key]
+                
+            })
+        },
+        resetMovies(state){
+            state.movies = []
+        }
+    },
+    actions:{
+        async searchMovies({commit}, payload){
+            const {title, type, number, year} = payload
+            const OMDB_API_KEY = '7035c60c' 
+            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+            const { Search, totalResults} = res.data
+            commit('updatestate',{
+                movies:Search
+            })
+        }
+    },
+}
