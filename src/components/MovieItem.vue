066675ac308a2b9fe36a02cd88fb1213aac39da9
chocosong2 <greentea030@naver.com>
2026-06-22 16:54:00 +0900

    commit

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>{{ movie.Title }}</div>
+  <div class="movie" 
+  :style="{backgroundImage: `url(${movie.Poster})`}">
+  <div class="info">
+    <div class="year">
+        {{ movie.Year }}
+    </div>
+    <div class="title">
+        {{ movie.Title }}
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +21,26 @@ export default {
         }
     }
 }
-
 </script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+.movie {
+    $width: 168px;
+    width: $width;
+    height: $width * 3/2;
+    margin: 10px;
+    border-radius: 4px;
+    background-color: $gray-400;
+    background-size:cover;
+    overflow: hidden;
+    position: relative;
+    .info {
+        position: absolute;
+        left:0;
+        bottom: 0;
+    }
+}
+
+
+</style>
