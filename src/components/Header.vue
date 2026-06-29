1a7fefb987d4b956c3d0744ac8f93d4730f40214
chocosong2 <greentea030@naver.com>
2026-06-29 16:45:57 +0900

    20260629

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,6 +13,9 @@
                 {{ nav.name }}
             </RouterLink>
         </div>
+        <div class="user" @click="toAbout">
+            <img :src="image" :alt="name">
+        </div>
     </div>
 </header>
 </template>
@@ -46,19 +49,58 @@ export default {
         isMatch(path){
             if(!path) return false 
             return path.test(this.$route.fullPath)
+        },
+        toAbout(){
+            this.$router.push('/about')
+        }
+    },
+    computed:{
+        image(){
+            return this.$store.state.about.image
+        },
+        name(){
+            return this.$store.state.about.name
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
 header {
     height: 70px;
     padding:0 40px;
     display: flex;
     align-items: center;
+    position: relative;i
     .logo{
         margin-right: 40px;
+    }
+    .user{
+        width: 40px;   
+        height: 40px;
+        padding:5px;
+        border-radius: 50%;
+        box-sizing: border-box;
+        background-color: $gray-200;
+        cursor: pointer;
+        position: absolute;
+        top:0;
+        bottom:0;
+        right:40px;
+        margin: auto;
+        &:hover{
+            transition: .4s;
+            background-color:darken($gray-200,10%);
+        }
+        img{
+            width: 100%;
+        }
+    }
+    @include media-breakpoint-down(sm){
+        .nav {
+            display: none;
+        }
     }
 }
 </style>
